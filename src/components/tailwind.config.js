ec6938979fd7dfa10590bc94465c34d445d23586
andrew <andrew7810262000@gmail.com>
2023-05-02 07:58:41 +0800

    add theme

--- src/components/tailwind.config.js
+++ src/components/tailwind.config.js
@@ -1,13 +1,22 @@
 const plugin = require("tailwindcss/plugin");
+const responsiveRegex = require("../lib/responsiveRegex"); // use for from-{color} and text-{color}
 module.exports = {
   content: [
-    { raw: '' },
+    {raw: ''},
   ],
+  safelist: responsiveRegex,
   theme: {
-    colors: require("tailwindcss/colors"),
+    colors: {
+      ...require("tailwindcss/colors"),
+    },
+    extend: {
+      colors: {
+        ...require("../colors/themes")["[data-theme=light]"],
+      }
+    }
   },
   plugins: [
-    plugin(function ({ addBase, addUtilities }) {
+    plugin(function ({addBase, addUtilities}) {
       addBase(require("../../dist/base"));
       // addUtilities(require("tailwindcss/utilities"));
     }),
