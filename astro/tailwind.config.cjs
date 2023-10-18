21ad4ec21b69cbe61668f5efee798c12e218ae17
andrew_wang <andrew7810262000@gmail.com>
2023-10-18 16:49:04 +0800

    [astro] safelist the bgColors avoid purging

--- astro/tailwind.config.cjs
+++ astro/tailwind.config.cjs
@@ -1,11 +1,18 @@
+const colors = require('../src/gen_default_theme.json');
+const bgColors = Object.entries(colors).map(([key,value]) => `bg-${key}`);
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 
+  // safelist: [
+  //   {pattern: /.*/}
+  // ],
+
   // use safelist to avoid purging tonic-ui classes
   safelist: [
-    {pattern: /.*/}
+    ...bgColors,
   ],
 
 	theme: {
