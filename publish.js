445b876e57ae85b691a4bccca13788e7be21d716
andrew_wang <andrew7810262000@gmail.com>
2023-10-20 07:08:02 +0800

    [github-action] fix publish.js error

--- publish.js
+++ publish.js
@@ -4,9 +4,13 @@ const path = require('path');
 const ghpages = require('gh-pages');
 const copyFile = require('./utils/copyFile');
 
+const repo = process.env.GH_TOKEN
+  ? `https://${process.env.GH_TOKEN}@adc.github.trendmicro.com/Consumer-WSE/MyAccount5-Laravel.git`
+  : 'git@adc.github.trendmicro.com:Consumer-Frontend/consumer-tonic-ui.git';
+
 const options = {
   branch: 'dist',
-  repo: 'git@adc.github.trendmicro.com:Consumer-Frontend/consumer-tonic-ui.git' // project github repo
+  repo // project github repo
 };
 
 const callback = err => {
@@ -14,12 +18,11 @@ const callback = err => {
   if (err) {
     console.error(err);
     process.exit(1); // exit node process with 'failure' code
-  }
-  else console.log('publish success');
+  } else console.log('publish success');
 };
 
 // for dist using
-copyFile(path.resolve(__dirname, 'dist-package.json'),path.resolve(__dirname,'dist', 'package.json'));
+copyFile(path.resolve(__dirname, 'dist-package.json'), path.resolve(__dirname, 'dist', 'package.json'));
 
 /**
  * This task pushes to the `master` branch of the configured `repo`.
