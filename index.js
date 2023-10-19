a07f1434585e610539611ff14962a22657e1a2ea
andrew_wang <andrew7810262000@gmail.com>
2023-10-20 06:28:56 +0800

    Updates

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 const colors = require('./colors/index');
 const themes = require('./colors/themes');
 const colorFunctions = require('./colors/functions.js');
-const base = require('../dist/base.js');
-const component = require('../dist/component.js');
+const base = require('./base.js');
+const component = require('./component.js');
 const {getColorObject} = require('./colors/index.js');
 
 // ref - https://github.com/saadeghi/daisyui/blob/master/src/index.js
@@ -26,7 +26,7 @@ const mainFunction = ({addBase, addComponents, addUtilities, config, postcss, e,
   // inject @base style
   addBase(base);
 
-  // inject components
+  // inject components - button
   addComponents(component);
 
   const themeInjector = colorFunctions.injectThemes(addBase, config, themes);
