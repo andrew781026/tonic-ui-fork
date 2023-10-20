b2c6c60a301e111597436006038e9d7af26c3a1b
andrew_wang <andrew7810262000@gmail.com>
2023-10-20 09:24:49 +0800

    [github-action] testing publish.js

--- publish.js
+++ publish.js
@@ -7,7 +7,8 @@ const copyFile = require('./utils/copyFile');
 console.log('process.env.GITHUB_TOKEN=', process.env.GITHUB_TOKEN);
 
 const repo = process.env.GITHUB_TOKEN
-  ? `https://git:${process.env.GITHUB_TOKEN}@adc.github.trendmicro.com/Consumer-Frontend/consumer-tonic-ui.git`
+  // ? `https://git:${process.env.GITHUB_TOKEN}@adc.github.trendmicro.com/Consumer-Frontend/consumer-tonic-ui.git`
+  ? `https://git:${process.env.GITHUB_TOKEN}@github.com/andrew781026/tonic-ui-fork.git`
   : 'git@adc.github.trendmicro.com:Consumer-Frontend/consumer-tonic-ui.git';
 
 const options = {
