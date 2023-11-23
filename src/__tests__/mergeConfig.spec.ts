df8266777b6c6c41705ae16a56133cfe0fae24ee
andrew_wang <andrew7810262000@gmail.com>
2023-11-23 14:12:34 +0800

    [test] add fontSize test-case

--- src/__tests__/mergeConfig.spec.ts
+++ src/__tests__/mergeConfig.spec.ts
@@ -114,4 +114,92 @@ describe('multiThemePlugin', () => {
     })
   })
 
+  describe('fontSize', () => {
+    it('idp extension config', () => {
+      const config: MultiThemePluginOptions = {
+        defaultTheme: {
+          name: 'defaultTheme',
+          extend: {
+            fontSize: {
+              xs: ['12px', '16px'],
+              sm: ['14px', '20px'],
+              base: ['16px', '24px'],
+              lg: ['18px', '28px'],
+              xl: ['20px', '28px'],
+              '2xl': ['24px', '32px'],
+              '2.5xl': ['28px', 'normal'],
+              '3xl': ['30px', '36px'],
+              '4xl': ['36px', '40px'],
+              '5xl': ['48px', 1],
+              '6xl': ['60px', 1],
+              '7xl': ['72px', 1],
+              '8xl': ['96px', 1],
+              '9xl': ['128px', 1],
+            },
+          }
+        },
+        themes: [
+          {
+            name: 'other',
+            extend: {
+              fontSize: {
+                xs: ['10px', '16px'],
+                sm: ['12px', '20px'],
+                base: ['14px', '24px'],
+                lg: ['16px', '28px'],
+                xl: ['18px', '28px'],
+                '2xl': ['22px', '32px'],
+                '2.5xl': ['26px', 'normal'],
+                '3xl': ['28px', '36px'],
+                '4xl': ['34px', '40px'],
+                '5xl': ['46px', 1],
+                '6xl': ['58px', 1],
+                '7xl': ['70px', 1],
+                '8xl': ['94px', 1],
+                '9xl': ['126px', 1],
+              },
+            }
+          },
+          {
+            name: 'extra',
+            extend: {
+              fontSize: {
+                xl: ['18px', '28px'],
+                '2,xl': ['22px', '32px'],
+                '2.5xl': ['26px', 'normal'],
+                '3+xl': ['28px', '36px'],
+                '4~xl': ['34px', '40px'],
+                '#5xl': ['46px', 1],
+                '6$xl': ['46px', 1],
+              },
+            }
+          },
+        ]
+      }
+
+      const expect01 = {
+          "xs": ["var(--fontSize-xs-0)", "var(--fontSize-xs-1-lineHeight)"],
+          "sm": ["var(--fontSize-sm-0)", "var(--fontSize-sm-1-lineHeight)"],
+          "base": ["var(--fontSize-base-0)", "var(--fontSize-base-1-lineHeight)"],
+          "lg": ["var(--fontSize-lg-0)", "var(--fontSize-lg-1-lineHeight)"],
+          "xl": ["var(--fontSize-xl-0)", "var(--fontSize-xl-1-lineHeight)"],
+          "2xl": ["var(--fontSize-\\32xl-0)", "var(--fontSize-\\32xl-1-lineHeight)"],
+          "2.5xl": ["var(--fontSize-\\32\\.5xl-0)", "var(--fontSize-\\32\\.5xl-1-lineHeight)"],
+          "3xl": ["var(--fontSize-\\33xl-0)", "var(--fontSize-\\33xl-1-lineHeight)"],
+          "4xl": ["var(--fontSize-\\34xl-0)", "var(--fontSize-\\34xl-1-lineHeight)"],
+          "5xl": ["var(--fontSize-\\35xl-0)", "var(--fontSize-\\35xl-1-lineHeight)"],
+          "6xl": ["var(--fontSize-\\36xl-0)", "var(--fontSize-\\36xl-1-lineHeight)"],
+          "7xl": ["var(--fontSize-\\37xl-0)", "var(--fontSize-\\37xl-1-lineHeight)"],
+          "8xl": ["var(--fontSize-\\38xl-0)", "var(--fontSize-\\38xl-1-lineHeight)"],
+          "9xl": ["var(--fontSize-\\39xl-0)", "var(--fontSize-\\39xl-1-lineHeight)"],
+          "2,xl": ["var(--fontSize-\\32\\2c xl-0)", "var(--fontSize-\\32\\2c xl-1-lineHeight)"],
+          "3+xl": ["var(--fontSize-\\33\\+xl-0)", "var(--fontSize-\\33\\+xl-1-lineHeight)"],
+          "4~xl": ["var(--fontSize-\\34\\~xl-0)", "var(--fontSize-\\34\\~xl-1-lineHeight)"],
+          "#5xl": ["var(--fontSize-\\#5xl-0)", "var(--fontSize-\\#5xl-1-lineHeight)"],
+          "6$xl": ["var(--fontSize-\\36\\$xl-0)", "var(--fontSize-\\36\\$xl-1-lineHeight)"]
+        };
+
+      expect(mergeConfig(config).fontSize).toStrictEqual(expect01);
+    })
+  })
 })
