cb6fc516430e610a6b414290eea1f9e9770d7594
andrew_wang <andrew7810262000@gmail.com>
2023-12-22 09:56:47 +0800

    [astro] add singleIcon page

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -5,9 +5,18 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import vue from '@astrojs/vue';
 
+import monacoEditorPlugin from 'vite-plugin-monaco-editor';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://adc.github.trendmicro.com',
   base: '/pages/Consumer-Frontend/consumer-tonic-ui',
-  integrations: [mdx(), sitemap(), tailwind(), vue()]
+  integrations: [mdx(), sitemap(), tailwind(), vue()],
+  vite: {
+    plugins: [
+      monacoEditorPlugin({
+        languages: ['css', 'html', 'json', 'typescript', 'javascript']
+      })
+    ],
+  }
 });
---
+++ astro/src/components/SingleIcon.vue
@@ -0,0 +1,62 @@
+<template>
+  <h2 class="text-3xl uppercase pb-2">{{ iconName }}</h2>
+  <div class="flex mx-auto gap-8">
+    <div
+      class="w-96 h-96 flex justify-center items-center border-2 border-solid border-tcsmd-ref-palette-gray-26 p-4 rounded-xl ">
+      <span :class="`${iconClazz} w-12 h-12`"></span>
+    </div>
+
+    <div
+      class="h-48 flex flex-col items-center border-2 border-solid border-tcsmd-ref-palette-gray-24 rounded-xl">
+      <div :class="`tcsmd-tab-group capsule`">
+        <div :class="`tcsmd-tab capsule`"
+             :selected="selectIndex == 1 ? true : null"
+             @click="selectIndex = 1"
+             tabindex="1">
+          HTML
+        </div>
+        <div :class="`tcsmd-tab capsule`"
+             :selected="selectIndex == 2 ? true : null"
+             @click="selectIndex = 2"
+             tabindex="1">
+          React
+        </div>
+        <div :class="`tcsmd-tab capsule`"
+             :selected="selectIndex == 3 ? true : null"
+             @click="selectIndex = 3"
+             tabindex="1">
+          Vue
+        </div>
+        <div :class="`tcsmd-tab capsule`"
+             :selected="selectIndex == 4 ? true : null"
+             @click="selectIndex = 4"
+             tabindex="1">
+          SVG
+        </div>
+      </div>
+      <div class="p-4 w-full">
+        <MonacoEditor :type="type"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {computed, ref} from "vue";
+import MonacoEditor from "./MonacoEditor.vue";
+
+defineProps(['iconClazz', 'iconName']);
+
+const selectIndex = ref<number>(1);
+
+const type = computed(() => {
+  const map = {
+    1: 'html',
+    2: 'react',
+    3: 'vue',
+    4: 'svg',
+  }
+
+  return map[selectIndex.value];
+})
+</script>
