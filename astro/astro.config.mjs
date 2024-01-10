c30233913b948377731fe49cc54353d7eab37084
andrew_wang <andrew7810262000@gmail.com>
2024-01-10 18:00:20 +0800

    ✏️[astro]: update doc site root

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -7,7 +7,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://adc.github.trendmicro.com',
-  base: '/pages/Consumer-Frontend/consumer-tonic-ui',
+  base: '/pages/Consumer-Frontend/consumer-tonic-ui/',
   integrations: [
     react(),
     vue(),
