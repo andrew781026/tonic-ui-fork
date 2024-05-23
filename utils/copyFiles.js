6c9b58a2468c6c22633cfdb2476bba11a5564896
andrew_wang <andrew7810262000@gmail.com>
2024-05-23 10:36:27 +0800

    🏗️feat(main): copyFiles.js change

--- utils/copyFiles.js
+++ utils/copyFiles.js
@@ -9,33 +9,15 @@ const copyFiles = (source, destination) => new Promise((resolve, reject) => {
   });
 })
 
-// npm run cp:error && npm run cp:themes && npm run cp:lib && npm run cp:root
-//   "cp:lib": "node utils/cp.js --src src/lib --dest dist/lib",
-//   "cp:themes": "node utils/cp.js --src src/themes --dest dist/themes",
-//   "cp:error": "node utils/cp.js --src src/error --dest dist/error",
-//   "cp:root": "node utils/cp.js --src src/index.js --dest dist/index.js",
-copyFiles('gen/lib', 'dist/lib')
-  .then(
-    () => copyFiles('gen/themes', 'dist/themes'),
-    console.error
-  )
-  .then(
-    () => copyFiles('gen/error', 'dist/error'),
-    console.error
-  )
-  .then(
-    () => copyFiles('gen/index.js', 'dist/index.js'),
-    console.error
-  )
-  .then(
-    () => copyFiles('gen/js', 'dist/js'),
-    console.error
-  )
-  .then(
-    () => copyFiles('src/img', 'dist/img'),
-    console.error
-  )
-  .then(
-    () => copyFiles('src/svg', 'dist/svg'),
-    console.error
-  )
+const runCopy = async () => {
+  await copyFiles('gen/lib', 'dist/lib');
+  await copyFiles('gen/themes', 'dist/themes');
+  await copyFiles('gen/error', 'dist/error');
+  await copyFiles('gen/index.js', 'dist/index.js');
+  await copyFiles('gen/js', 'dist/js');
+  await copyFiles('src/img', 'dist/img');
+  await copyFiles('src/svg', 'dist/svg');
+}
+
+runCopy()
+  .catch(console.error);
