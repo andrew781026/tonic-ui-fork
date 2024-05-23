f4dcfc22cfbc3e24220e7445ffc3b43609eba5ac
andrew_wang <andrew7810262000@gmail.com>
2024-05-23 10:27:25 +0800

    ✨[main] add js folder as export

--- utils/copyFiles.js
+++ utils/copyFiles.js
@@ -10,7 +10,7 @@ const copyFiles = (source, destination) => new Promise((resolve, reject) => {
 })
 
 // npm run cp:error && npm run cp:themes && npm run cp:lib && npm run cp:root
-// "cp:lib": "node utils/cp.js --src src/lib --dest dist/lib",
+//   "cp:lib": "node utils/cp.js --src src/lib --dest dist/lib",
 //   "cp:themes": "node utils/cp.js --src src/themes --dest dist/themes",
 //   "cp:error": "node utils/cp.js --src src/error --dest dist/error",
 //   "cp:root": "node utils/cp.js --src src/index.js --dest dist/index.js",
@@ -25,6 +25,10 @@ copyFiles('gen/lib', 'dist/lib')
   )
   .then(
     () => copyFiles('gen/index.js', 'dist/index.js'),
+    console.error
+  )
+  .then(
+    () => copyFiles('gen/js', 'dist/js'),
     console.error
   )
   .then(
