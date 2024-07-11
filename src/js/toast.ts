fd7395757a28851b46ca7436dd7d2e25dee9e467
andrew_wang <andrew7810262000@gmail.com>
2024-07-11 13:42:03 +0800

    🐛[main] fix openToast

--- src/js/toast.ts
+++ src/js/toast.ts
@@ -11,13 +11,34 @@ enum Position {
   BottomRight = "bottom-right",
 }
 
-export const openToast = (ele: HTMLElement, position: Position, delay = 2000) => {
+interface openToastParams {
+  el: HTMLElement,
+  position: Position,
+  delay: number,
+  offset: Record<any, any> | string
+}
+
+const setOffset = (target: HTMLElement, position: Position, offset: Record<any, any> | string) => {
+
+  // offset = { top:'4px', bottom:'4px', left:'4px', right:'4px' }
+  // offset = '4px'
+
+  const offsetObj = (typeof offset === 'string') ? {top: offset, bottom: offset, left: offset, right: offset} : offset;
+  if (position.includes('top') && offsetObj.top) target.style.top = offsetObj.top;
+  if (position.includes('left') && offsetObj.left) target.style.left = offsetObj.left;
+  if (position.includes('bottom') && offsetObj.bottom) target.style.bottom = offsetObj.bottom;
+  if (position.includes('right') && offsetObj.right) target.style.right = offsetObj.right;
+}
+
+export const openToast = ({el, position, delay = 2000, offset}: openToastParams) => {
 
   clearTimeout(timeoutId);
-  const toastEl = ele;
+  const toastEl = el;
   if (!toastEl) return;
 
   toastEl.className = `fixed z-10 toast gap-1 rounded black ${position}`;
+  setOffset(toastEl, position, offset);
+
   document.body.append(toastEl); // make element at root of body
   timeoutId = setTimeout(() => toastEl.classList.add('fade-in'));
 
